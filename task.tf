27b4f303cbe12728b07a5c447337c915e9bde4d7
Gunish <talk.gunish@gmail.com>
2020-06-17 20:33:23 +0530

    Creating Cloudfront and attaching S3 bucket to it.

--- task.tf
+++ task.tf
@@ -188,3 +188,93 @@ locals {
   s3_origin_id = "aws_s3_bucket.task-1-s3bucket.id"
 }
 
+#Creating Cloudfront and attaching S3 bucket to it.
+
+resource "aws_cloudfront_origin_access_identity" "o" {
+     comment = "hello"
+ }
+
+resource "aws_cloudfront_distribution" "task-1-s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.task-1-s3bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+    s3_origin_config {
+           origin_access_identity = aws_cloudfront_origin_access_identity.o.cloudfront_access_identity_path 
+     }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "terraform.png"
+
+  logging_config {
+    include_cookies = false
+    bucket          =  aws_s3_bucket.task-1-s3bucket.bucket_domain_name
+    
+  }
+
+
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "/content/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+   
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["US", "IN","CA", "GB", "DE"]
+    }
+  }
+
+  tags = {
+    Environment = "production"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+output "out3" {
+        value = aws_cloudfront_distribution.task-1-s3_distribution.domain_name
+}
